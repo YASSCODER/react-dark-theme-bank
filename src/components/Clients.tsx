2c3c2854cc5d47d415c5fe5c3a46325190c72a57
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2023-07-11 09:33:42 +0100

    creating client section : add clients logo using map on Clients array

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
-function Clients() {
+interface imgProps {
+  id: string,
+  logo: string
+}
+
+const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => 
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[129px] min-w-[120px]`}>
+            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain  hover:bg-gray-900"/>
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
